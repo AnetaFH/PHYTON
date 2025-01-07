40b237a41af9abd664ba27f9abf3b6ce73e7bf8b
AnetaFH <aneta.hlavackova@skanska.cz>
2025-01-07 21:41:24 +0100

    zmeny souboru

--- 241017_1_cviceni/1_cviceni.py
+++
@@ -1,5 +0,0 @@
-slovo = "indexovani"
-print(type(slovo))
-print(slovo[0:5])
-print(slovo[-5:])
-print(slovo[0::3])
--- 241017_1_cviceni/4_cviceni.py
+++
@@ -1,7 +0,0 @@
-veta = 'Zvuk řeči je produkován poměrně otevřenou konfigurací vokálního traktu'
-samohlasky = 'aeiouáéíóú'
-souhlasky = 'bcčdďfghjklmnňprřsštťvzžcdž'
-vysledek = {'souhlasky':0, 'samohlasky':0}
-print(vysledek)
-
-
